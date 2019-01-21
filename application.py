45f81f2163f611a052de9539aa2c9dacf58ebf10
ema <mrauto56@gmail.com>
2019-01-21 20:26:14 +0100

    removed session stuff

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ from tempfile import mkdtemp
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
